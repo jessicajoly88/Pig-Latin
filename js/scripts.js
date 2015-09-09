8c722aa889b881737314a89e1ce9e7a2c335dee6
rachael berecka & jessica joly <student@epicodus.com>
2015-09-09 12:21:10 -0700

    Third spec pass to account for a block of consonants at beginning of word

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,19 @@ var pigLatin = function(word) {
   var vowels = ["a", "e", "i", "o", "u"];
   var letters = word.split("");
   var first_letter = letters[0];
-
+console.log(first_letter)
   if (vowels.indexOf(first_letter) !== -1) {
     return word.concat("ay");
   } else {
-    letters.push(letters.shift());
-    var newWord = letters.join("");
-    var pigWord = newWord.concat("ay");
-    return pigWord;
+        do {
+          letters.push(letters.shift());
+          first_letter = letters[0];
+console.log(letters)
+          }
+        while(vowels.indexOf(first_letter) === -1);
+
+        var newWord = letters.join("");
+        var pigWord = newWord.concat("ay");
+        return pigWord;
   }
 };
--- specs/specs.js
+++ specs/specs.js
@@ -2,7 +2,12 @@ describe('pigLatin', function() {
   it("adds the letters ay to the end of a word that starts with a vowel", function() {
     expect(pigLatin("orange")).to.equal("orangeay");
   });
+
   it("if word begins with consonant, it removes the consonant and moves it to the end of the word + ay", function(){
     expect(pigLatin("red")).to.equal("edray");
+  });
+
+  it("removes the block of consonants found at the beginning of a word and adds it to the end plus adds ay", function(){
+    expect(pigLatin("brown")).to.equal("ownbray");
   });
 });
